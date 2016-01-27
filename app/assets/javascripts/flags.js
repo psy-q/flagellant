d8a8f63cc7a7ff0717f8d867fc75e41ae8164868
Ramón Cahenzli <rca@psy-q.ch>
2016-01-27 17:45:05 +0100

    Map color picker to correct properties.

--- app/assets/javascripts/flags.js
+++ app/assets/javascripts/flags.js
@@ -3,19 +3,19 @@
 $( document ).ready(function() {
     $('input.colorpicker').each(
         function() {
-        // Why can't I get at the data.altfield attributes that I'm interested in?
-        console.log(this.attributes);
+            var altField = this.dataset.altfield;
+            var altProperties = this.dataset.altproperties;
+            $(this).colorpicker({
+                altField:      altField,
+                altProperties: altProperties,
+                buttonImage:    '/assets/images/ui-colorpicker.png',
+                colorFormat:    '#HEX',
+                parts:          'full',
+                alpha:          false,
+                showOn:         'both',
+                buttonColorize: true,
+                showNoneButton: true
+            });
         }
     );
-    $('input.colorpicker').colorpicker({
-        //altField:      data.altfield,
-        //altProperties: data.altproperties,
-        buttonImage:    '/assets/images/ui-colorpicker.png',
-        colorFormat:    '#HEX',
-        parts:          'full',
-        alpha:          false,
-        showOn:         'both',
-        buttonColorize: true,
-        showNoneButton: true
-    });
 });
--- app/models/flag_layer.rb
+++ app/models/flag_layer.rb
@@ -1,5 +1,9 @@
 class FlagLayer < ActiveRecord::Base
   belongs_to :flag
   serialize :target_css
+
+  # Each color can have its own target property. Use the SVG CSS properties:
+  #  - fill
+  #  - stroke
   serialize :target_property
 end
