c3aba5094c3fd2750ee5b0d8ec83d178bb33b4bf
Ramón Cahenzli <rca@psy-q.ch>
2016-01-30 13:07:31 +0100

    Renaming for more clarity. Including sortorder field.

--- app/helpers/flag_layers_helper.rb
+++ app/helpers/flag_layers_helper.rb
@@ -1,13 +1,13 @@
 module FlagLayersHelper
 
-  def color_picker_field(layer, color, property = "fill")
+  def color_picker_field(layer_form, color, property = "fill")
     if property == "stroke"
-      layer.text_field(color.to_sym,  :size => 7, :class => 'colorpicker',
-                     :data => { :altfield => "#flag_layer_#{layer.object.id} .#{color}",
-                                :altproperties => 'stroke'} )
+      layer_form.text_field(color.to_sym,  :size => 7, :class => 'colorpicker',
+                            :data => { :altfield => "#flag_layer_#{layer_form.object.id} .#{color}",
+                                       :altproperties => 'stroke'} )
     else
-      layer.text_field(color.to_sym,  :size => 7, :class => 'colorpicker',
-                     :data => { :altfield => "#flag_layer_#{layer.object.id} .#{color}"} )
+      layer_form.text_field(color.to_sym,  :size => 7, :class => 'colorpicker',
+                            :data => { :altfield => "#flag_layer_#{layer_form.object.id} .#{color}"} )
     end
   end
 end
